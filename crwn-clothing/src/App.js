805f063dd7380fd4c8b05ff385c5e787544be9ef
Yukako Sunabe <yukakosunabe@gmail.com>
2019-11-26 16:43:15 -0800

    created project with create react app and added files for homepage

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import HomePage from "./homepage.component";
+import "./homepage.styles.scss";
+
+function App() {
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
---
+++ crwn-clothing/src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HAT</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+export default HomePage;
